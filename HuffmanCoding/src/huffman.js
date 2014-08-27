ae40dc38e300fc04e36590abc520efb314f398be
concreted <arichuang@gmail.com>
2014-08-27 10:35:44 -0700

    Implement makeHuffmanTree

--- HuffmanCoding/src/huffman.js
+++ HuffmanCoding/src/huffman.js
@@ -1,7 +1,14 @@
 // // Given a Huffman tree and a string, encode that string into a new string
 // // consisting only of 1s and 0s, using the code given by the tree.
 var encodeString = function(input, huffmanTree) {
-  return "10101";
+    console.log(huffmanTree);
+    var result = '';
+
+    for (var i = 0; i < input.length; i++) {
+	var current = huffmanTree	
+    }
+
+    return 
 };
 
 // // Given a Huffman tree and a string of 1s and 0s, decode that string into
@@ -22,6 +29,41 @@ var decodeString = function(input, huffmanTree) {
 // You may also use the `Tree` class that is provided in the file `misc.js`
 // Some corpuses are included as the variables `lorumIpsum` and `declaration`.
 var makeHuffmanTree = function(corpus) {
-  return new Tree();
+    var chars = corpus.split('');
+    var freqs = {};
+    for (var i = 0; i < chars.length; i++) {
+	if (freqs[chars[i]] === undefined) {
+	    freqs[chars[i]] = 1;
+	} 
+	else {
+	    freqs[chars[i]] += 1;
+	}
+    }
+
+    var pq = new PriorityQueue;
+
+    for (var c in freqs) {
+	var tree = new Tree([c]);
+	pq.insert(freqs[c], tree);
+    }
+    
+    while (pq.size() > 1) {
+	var a = pq.extract();
+	var b = pq.extract();
+
+	var totalFreq = a.key + b.key;
+
+
+	var newNode = new Tree(a.val.val.concat(b.val.val));
+
+	newNode.left = a.val;
+	newNode.right = b.val;
+
+	pq.insert(totalFreq, newNode);
+    }
+
+    console.log(pq.peek().val);
+    
+    return pq.peek().val;
 };
 
